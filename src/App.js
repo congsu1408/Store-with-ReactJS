fcbbd954cf8684675fea0bd4dd7f3823457419f7
congsu1408 <congsu196@gmail.com>
2024-04-01 00:34:51 +0700

    feat: design the main navigation bar

--- src/App.js
+++ src/App.js
@@ -4,23 +4,25 @@ import Home from "./Components/Home";
 import Privacy from "./Components/Privacy";
 import Signup from "./Components/Signup";
 import Loginform from "./Components/Loginform";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Navbar from "./Components/Navbar";
+import './App.css';
+
 const App = () => {
-  return (
-    <div>
-        <Router>
-            <Navbar />
-            <Routes>
-                <Route path={'/'} element={<Home />} />
-                <Route path={'/contact'} element={<Contact />} />
-                <Route path={'/privacy'} element={<Privacy />} />
-                <Route path={'/signup'} element={<Signup />} />
-                <Route path={'/loginform'} element={<Loginform />} />
-            </Routes>
-        </Router>
-    </div>
-  );
+    return (
+        <div>
+            <Router>
+                <Navbar/>
+                <Routes>
+                    <Route path={'/'} element={<Home/>}/>
+                    <Route path={'/contact'} element={<Contact/>}/>
+                    <Route path={'/privacy'} element={<Privacy/>}/>
+                    <Route path={'/signup'} element={<Signup/>}/>
+                    <Route path={'/loginform'} element={<Loginform/>}/>
+                </Routes>
+            </Router>
+        </div>
+    );
 };
 
 export default App;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,17 +1,46 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
+import Imgrefresher from './images/logo.avif';
+import './Navbar.css';
+import SearchIcon from '@mui/icons-material/Search';
+import CloseIcon from '@mui/icons-material/Close';
 
 const Navbar = () => {
-  return (
-    <div>
-        <Link to={'/'}>Home</Link>
-        <Link to={'/signup'}>Signup</Link>
-        <Link to={'/contact'}>Contact</Link>
-        <Link to={'/privacy'}>Privacy</Link>
-        <Link to={'/loginform'}>Loginform</Link>
-
-    </div>
-  );
+    return (
+        <div>
+            <div className={"navbar_div"}>
+                <div>
+                    <input type="text" placeholder={'Search Here'}/>
+                    <SearchIcon className={"search_icon_input"}/>
+                    <CloseIcon className={"close_icon"}/>
+                </div>
+                <div>
+                    <div className={"navbar_main"}>
+                        <div>
+                            <div>
+                                <SearchIcon className={"search_icon"}/>
+                            </div>
+                            {/*Menu Icon*/}
+                        </div>
+                        <div>
+                            <img src={Imgrefresher} className={"heading_logo"} alt=""/>
+                        </div>
+                        <div className="bad_search">
+                            {/*search icon*/}
+                            {/*local icon*/}
+                        </div>
+                    </div>
+                    <div className="Nav_links">
+                        <Link to={'/'} className="link">Home</Link>
+                        <Link to={'/signup'} className={"link"}>Signup</Link>
+                        <Link to={'/contact'} className={"link"}>Contact</Link>
+                        <Link to={'/privacy'} className={"link"}>Privacy</Link>
+                        <Link to={'/loginform'} className={"link"}>Loginform</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Navbar;
